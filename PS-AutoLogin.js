672ff4a59e74c12bb3c8f9951439f3ad83f15c1e
K7 <k7atwork@users.noreply.github.com>
2016-07-07 14:32:55 +0930

    Update PS-AutoLogin.js

--- PS-AutoLogin.js
+++ PS-AutoLogin.js
@@ -1,3 +1,5 @@
 # titBits
 
-javascript:function%20E(){%20f0=document.forms[0];f0['userid'].value='a123456';%20f0['pwd'].value='password';%20f0['Submit'].click();%20}E()
+javascript:function%20E(){f0=document.forms[0];f0['userid'].value='a123456';f0['pwd'].value='password';f0['Submit'].click();%20}E()
+
+javascript:function%20E(){var%20usrid=prompt("User%20ID","a...");f0=window('dv2wrklst').document.forms[0];f0['userid'].value=usrid;f0['pwd'].value='Test@123';f0['Submit'].click();}E()
