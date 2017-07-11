d72fd3611adecb89bd0c4f7e3c0707aea4b16a72
K7 <k7atwork@users.noreply.github.com>
2017-07-11 10:39:26 +0930

    Create gridHeaderFloating.js

---
+++ gridHeaderFloating.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+var gridHdr=document.getElementById("divghrcG_PAGE_NAV$0");
+gridHdr.style.cssText += ' position: fixed; top: 60px; z-index: 20';
+var gridBody=document.getElementById("divgbG_PAGE_NAV$0");
+gridBody.style.cssText += ' position: absolute; top: 100px; z-index: 19';
+</script>
