9995f8a5700a7ea7359ed00e8254555eb80301f4
K7 <k7atwork@users.noreply.github.com>
2017-09-11 10:06:25 +0930

    Update calendarHighlight-HTML.js

--- calendarHighlight-HTML.js
+++ calendarHighlight-HTML.js
@@ -2,14 +2,15 @@
 
 <script type="text/javascript">
 
-var scriptElement = document.createElement('script');
-scriptElement.setAttribute('language','javascript');
-scriptElement.type = 'text/javascript';
-scriptElement.setAttribute('src','%BIND(:1)');
-scriptElement.innerText = '';
+  var scriptElement = document.createElement('script');
+  scriptElement.setAttribute('language','javascript');
+  scriptElement.type = 'text/javascript';
+  scriptElement.setAttribute('src','%BIND(:1)');
+  scriptElement.innerText = '';
 
-document.getElementsByTagName('head')[0].appendChild(scriptElement);
+  document.getElementsByTagName('head')[0].appendChild(scriptElement);
 
-var calprompt = document.getElementById('UOA_FWA_SPK_WRK_BEGIN_DT$prompt').href += ' highlightDates(%BIND(:2));';
+  var calprompt1 = document.getElementById('UOA_FWA_SPK_WRK_BEGIN_DT$prompt').href += ' highlightDates(%BIND(:2));';
+  var calprompt2 = document.getElementById('UOA_FWA_SPK_WRK_END_DT$prompt').href += ' highlightDates(%BIND(:2)+4);';
 
 </script>
