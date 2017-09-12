838d5773f5ee1ce25f7b9e3e22f0c9b152e337a9
K7 <k7atwork@users.noreply.github.com>
2017-09-12 10:42:49 +0930

    Update calendarHighlight-HTML.js

--- calendarHighlight-HTML.js
+++ calendarHighlight-HTML.js
@@ -12,10 +12,12 @@
 
 //  var calprompt1 = document.getElementById('G_FORM_VFWA_BEGIN_DT$prompt').href += ' highlightDates(%BIND(:2));';
   var calprompt1 = document.getElementById('G_FORM_VFWA_BEGIN_DT$prompt');
-  if (calprompt1 != null) calprompt1.href += ' highlightDates(%BIND(:2));';
+  if ((calprompt1 != null) && (calprompt1.href.indexOf('highlightDates(') == -1))
+    calprompt1.href += ' highlightDates(%BIND(:2));';
 
 //  var calprompt2 = document.getElementById('G_FORM_VFWA_END_DT$prompt').href += ' highlightDates(%BIND(:2)+4);';
   var calprompt2 = document.getElementById('G_FORM_VFWA_END_DT$prompt');
-  if (calprompt2 != null) calprompt2.href += ' highlightDates(%BIND(:2)+4);';
+  if ((calprompt2 != null) && (calprompt2.href.indexOf('highlightDates(') == -1))
+    calprompt2.href += ' highlightDates(%BIND(:2)+4);';
 
 </script>
